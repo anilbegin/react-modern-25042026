e0b4b1f9b5206cdeddb40805dd2216c08b6e63bb
anilbegin <98578895+anilbegin@users.noreply.github.com>
2026-05-27 11:46:41 +0530

    without markdown toolbar

--- app/components/EditPost.js
+++ app/components/EditPost.js
@@ -1,13 +1,19 @@
-import React, {useState, useEffect} from "react"
-import { useParams, Link } from "react-router-dom"
+import React, {useState, useEffect, useContext} from "react"
+import { useParams, Link, useNavigate } from "react-router-dom"
 import { useImmerReducer } from "use-immer"
 import Axios from 'axios'
 
 import LoadingDotsIcon from "./LoadingDotsIcon"
+import StateContext from "../StateContext"
+import DispatchContext from "../DispatchContext"
 import Page from "./Page"
 
 function EditPost() {
-  const originalState = {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
+  const navigate = useNavigate()
+
+  const initialState = {
     title: {
       value: "",
       hasErrors: false,
@@ -24,6 +30,8 @@ function EditPost() {
     sendCount: 0
   }
 
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+
   function ourReducer(draft, action) {
     switch(action.type) {
       case "fetchComplete":
@@ -31,11 +39,25 @@ function EditPost() {
         draft.body.value = action.value.body
         draft.isFetching = false
         return
+      case "titleChange" :
+        draft.title.value = action.value
+        return
+      case "bodyChange" :
+        draft.body.value = action.value
+        return
+      case "saveChanges" :
+        draft.sendCount++
+        return
+      case "saveRequestStarted" :
+        draft.isSaving = true
+        return
+      case "saveRequestFinished" :
+        draft.isSaving = false
+        return          
     }
   }
-  
-  const [state, dispatch] = useImmerReducer(ourReducer, originalState)
-
+    
+  // fetching the post by ID on initial Page Load
   useEffect(() => {
     const ourRequest = new AbortController()
     async function fetchPost() {
@@ -57,8 +79,36 @@ function EditPost() {
     return () => ourRequest.abort() 
   } , [])
 
+  // saveChanges - Send Request to Update the Post
+  useEffect(() => {
+    if(state.sendCount > 0) {
+      dispatch({type: 'saveRequestStarted'})
+      async function updatePost() {
+        try {
+          const response = await Axios.post(`/post/${state.id}/edit`, {
+            title: state.title.value,
+            body: state.body.value, 
+            token: appState.user.token
+          })
+          dispatch({type: 'saveRequestFinished'})
+          //  console.log(response) // response.data - success/failure
+          if(response.data == 'success') {
+            appDispatch({type: 'flashMessage', value: 'Post Updated Successfully.'})
+          //  navigate(`/post/${state.id}`)
+          } else {
+            appDispatch({type: 'flashMessage', value: 'Update Failed! Please try again.'})
+          }
+        } catch (e) {
+          console.log(e)
+        }
+      }
+      updatePost()
+    }
+  } , [state.sendCount])
+
   function handleEdit(e) {
     e.preventDefault()
+    dispatch({type: "saveChanges"})
   }
 
   if(state.isFetching) return (
@@ -76,17 +126,16 @@ function EditPost() {
               <label htmlFor="post-title" className="text-muted mb-1">
                 <small>Title</small>
               </label>
-              <input value={state.title.value} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
+              <input value={state.title.value} onChange={e => dispatch({type:"titleChange", value: e.target.value})} autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" />
             </div>
 
             <div className="form-group">
               <label htmlFor="post-body" className="text-muted mb-1 d-block">
                 <small>Body Content</small>
               </label>
-              <textarea value={state.body.value} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" />
+              <textarea value={state.body.value} onChange={e => dispatch({type: "bodyChange", value: e.target.value})} name="body" id="post-body" className="body-content tall-textarea form-control" type="text" />
             </div>
-          
-            <button className="btn btn-success">Save Changes</button>
+            <button className="btn btn-success" disabled={state.isSaving}>{state.isSaving ? 'Saving' : 'Save Changes'}</button>
           </form>
         </div>
       </main>
