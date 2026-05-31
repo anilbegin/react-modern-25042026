51edd8896049a1ea58d5928ac43aab06886a74a4
anilbegin <98578895+anilbegin@users.noreply.github.com>
2026-05-31 16:34:21 +0530

    corrections in lines

--- app/components/NotFound.js
+++ app/components/NotFound.js
@@ -10,7 +10,8 @@ function NotFound() {
         <div className="container container--narrow py-md-5">
           <div className="text-center">
             <h2>Whoops, we cannot find that page.</h2>
-            <p className="lead text-muted">You can always visit the <Link to="/">homepage</Link> to get a fresh start.</p>
+            <p className="lead text-muted">
+              You can always visit the <Link to="/">homepage</Link> to get a fresh start.</p>
           </div>
         </div>
       </main>
