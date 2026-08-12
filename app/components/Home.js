6e57c78c47c22c8d0eff57e363093defe6c419ed
anilbegin <98578895+anilbegin@users.noreply.github.com>
2026-08-12 18:42:34 +0530

    display num of posts published on empty feed homepage for logged In user with 0 followings

--- app/components/Home.js
+++ app/components/Home.js
@@ -13,9 +13,11 @@ function Home() {
   const appDispatch = useContext(DispatchContext)
   const [state, setState] = useImmer({
     isLoading: true,
-    feed: []
+    feed: [],
+    postsCount: 0
   })
 
+  // fetch all the latest posts from all "Followings" of the Current loggedIn User 
   useEffect(() => {
     const ourRequest = new AbortController()
     async function fetchPosts() {
@@ -38,6 +40,28 @@ function Home() {
     
     return () => ourRequest.abort() 
   }, [])
+
+  // retrieve the number of posts created by the Logged In User
+  useEffect(() => {
+    const ourRequest = new AbortController()
+    async function fetchPosts() {
+      try {
+        const response = await Axios.get(`/profile/${appState.user.username}/posts`, {
+          signal : ourRequest.signal
+        })
+        //console.log(response.data)
+        if(response.data) {
+          setState(draft => {
+            draft.postsCount = response.data.length
+          })
+        }
+      } catch (e) {
+        console.log(e)
+      } 
+    }
+    fetchPosts()
+    return () => ourRequest.abort()
+  } , [])
 
   function handleSearchIcon(e) {
     e.preventDefault()
@@ -76,13 +100,16 @@ function Home() {
             </div>
             <h2 className="text-center mb-3">Hello <strong>{appState.user.username}</strong>, your feed is empty.</h2>
             <p className="lead text-muted text-center mx-auto">Your feed displays the latest posts from the people you follow. If you don&rsquo;t have any friends to follow that&rsquo;s okay; you can use the &ldquo;Search&rdquo; feature in the top menu bar to find content written by people with similar interests and then follow them.</p>
-            { /** new actions */ } 
+            { /** includes CTA for New Users */ } 
             <div className="mt-4">
+              {(state.postsCount > 0 ? <p className="text-muted mb-3">
+                <strong>{state.postsCount}</strong> posts published by you.
+              </p> : "")}
               <a onClick={handleSearchIcon} href="#" className="btn btn-success mr-2">
               <i className="fas fa-search mr-1"></i> Search Posts
               </a>
               <Link to="/create-post" className="btn btn-outline-secondary mt-2 mt-md-0">
-                <i className="fas fa-pen mr-1"></i> Write Your First Post
+                <i className="fas fa-pen mr-1"></i> {state.postsCount > 0 ? "Create a New Post": "Write Your First Post"}
               </Link>
             </div>
           </div>  
