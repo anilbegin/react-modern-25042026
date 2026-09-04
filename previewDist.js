032e0e9e8beec4d3aec82a7953b19949607402f6
anilbegin <98578895+anilbegin@users.noreply.github.com>
2026-09-04 16:51:37 +0530

    building dist Folder Part-2 - preview Dist copy in Browser

---
+++ previewDist.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const path = require("path")
+const app = new express()
+app.use(express.static(path.join(__dirname, "dist")))
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist","index.html"))
+})
+app.listen("4000")
